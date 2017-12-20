5635b0ad07622cbf8efaede95129e01addf6fa29
R422 <R422@jec.ac.jp>
2017-12-20 12:28:47 +0900

    バーの移動を変更

--- Breakout/Assets/Script/Ball.cs
+++ Breakout/Assets/Script/Ball.cs
@@ -3,16 +3,16 @@ using System.Collections;
 
 public class Ball : MonoBehaviour
 {
-    private float speed = 10.0f;    //これを追加
-   
+    private float speed = 15.0f;    //これを追加
+  
 
     // Use this for initialization
     void Start()
-    {
-        this.GetComponent<Rigidbody>().AddForce(
-            (transform.right + transform.up) * speed,
-            ForceMode.VelocityChange);
-
+    { 
+        
+           this.GetComponent<Rigidbody>().AddForce(
+           (transform.right + transform.up) * speed,
+           ForceMode.VelocityChange);
     }
     private void OnCollisionEnter(Collision collision)
     {
@@ -21,15 +21,32 @@ public class Ball : MonoBehaviour
         {
             Destroy(collision.gameObject);
         }
-        
-        //if (Mathf.Abs(this.GetComponent<Rigidbody>().velocity.y) < 3)
-        //{
-        //    this.GetComponent<Rigidbody>().velocity.y
-        //}
+        if (collision.gameObject.tag == "WallBottom")
+        {
+            Destroy(collision.gameObject);
+        }
+      
     }
     // Update is called once per frame
     void Update()
     {
-
+        //反射角度の修正
+        if (Mathf.Abs(this.GetComponent<Rigidbody>().velocity.y) < 3)
+        {
+            this.GetComponent<Rigidbody>().AddForce(
+            (transform.right + transform.up) * speed,
+            ForceMode.VelocityChange);
+        }
+        if (Mathf.Abs(this.GetComponent<Rigidbody>().velocity.x) < 3)
+        {
+            this.GetComponent<Rigidbody>().AddForce(
+            (transform.right + transform.up) * speed,
+            ForceMode.VelocityChange);
+        }
+        //画面下の出たら
+        if(this.GetComponent<Transform>().position.y <= -40)
+        {
+            //まだ
+        }
     }
 }
--- Breakout/Assets/Script/Bar.cs
+++ Breakout/Assets/Script/Bar.cs
@@ -4,9 +4,9 @@ using System.Collections;
 public class Bar : MonoBehaviour
 {
     // 位置座標
-    private Vector3 position;
-    // スクリーン座標をワールド座標に変換した位置座標
-    private Vector3 screenToWorldPointPosition;
+    //private Vector3 position;
+    //// スクリーン座標をワールド座標に変換した位置座標
+    //private Vector3 screenToWorldPointPosition;
     // Use this for initialization
     void Start()
     {
@@ -16,14 +16,24 @@ public class Bar : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Vector3でマウス位置座標を取得する
-        position = Input.mousePosition;
-        // Z軸修正
-        position.z = -Camera.main.transform.position.z;
-        position.y = -Camera.main.transform.position.y + 60;
-        // マウス位置座標をスクリーン座標からワールド座標に変換する
-        screenToWorldPointPosition = Camera.main.ScreenToWorldPoint(position);
-        // ワールド座標に変換されたマウス座標を代入
-        gameObject.transform.position = screenToWorldPointPosition;
+        //// Vector3でマウス位置座標を取得する
+        //position = Input.mousePosition;
+        //// Z軸修正
+        //position.z = -Camera.main.transform.position.z;
+        //position.y = -Camera.main.transform.position.y + 70;
+       
+        //// マウス位置座標をスクリーン座標からワールド座標に変換する
+        //screenToWorldPointPosition = Camera.main.ScreenToWorldPoint(position);
+        //// ワールド座標に変換されたマウス座標を代入
+        //gameObject.transform.position = screenToWorldPointPosition;
+
+        if(Input.GetKey(KeyCode.RightArrow) && transform.position.x <= 9)
+        {
+            transform.position += new Vector3(10.0f, 0.0f, 0.0f) * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) && transform.position.x >= -9)
+        {
+            transform.position += new Vector3(-10.0f, 0.0f, 0.0f) * Time.deltaTime;
+        }
     }
 }
