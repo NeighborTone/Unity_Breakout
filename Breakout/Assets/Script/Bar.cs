14a0765c8805b8bdfc76ca91b3c9ce7912fe0c05
R821 <R821@jec>
2017-12-20 17:22:25 +0900

    ブロックをスクリプトで並べた

--- Breakout/Assets/Script/Bar.cs
+++ Breakout/Assets/Script/Bar.cs
@@ -3,11 +3,7 @@ using System.Collections;
 
 public class Bar : MonoBehaviour
 {
-    // 位置座標
-    //private Vector3 position;
-    //// スクリーン座標をワールド座標に変換した位置座標
-    //private Vector3 screenToWorldPointPosition;
-    // Use this for initialization
+
     void Start()
     {
 
@@ -16,24 +12,14 @@ public class Bar : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //// Vector3でマウス位置座標を取得する
-        //position = Input.mousePosition;
-        //// Z軸修正
-        //position.z = -Camera.main.transform.position.z;
-        //position.y = -Camera.main.transform.position.y + 70;
        
-        //// マウス位置座標をスクリーン座標からワールド座標に変換する
-        //screenToWorldPointPosition = Camera.main.ScreenToWorldPoint(position);
-        //// ワールド座標に変換されたマウス座標を代入
-        //gameObject.transform.position = screenToWorldPointPosition;
-
-        if(Input.GetKey(KeyCode.RightArrow) && transform.position.x <= 9)
+        if(Input.GetKey(KeyCode.RightArrow) && transform.position.x <= 10)
         {
-            transform.position += new Vector3(10.0f, 0.0f, 0.0f) * Time.deltaTime;
+            transform.position += new Vector3(20.0f, 0.0f, 0.0f) * Time.deltaTime;
         }
-        if (Input.GetKey(KeyCode.LeftArrow) && transform.position.x >= -9)
+        if (Input.GetKey(KeyCode.LeftArrow) && transform.position.x >= -10)
         {
-            transform.position += new Vector3(-10.0f, 0.0f, 0.0f) * Time.deltaTime;
+            transform.position += new Vector3(-20.0f, 0.0f, 0.0f) * Time.deltaTime;
         }
     }
 }
---
+++ Breakout/Assets/Script/BlockMap.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockMap : MonoBehaviour
+{
+
+    public GameObject blockPrefab;
+    public int placeX;          ////横に並べる個数
+    public int placeY;          ////縦に並べる個数
+    public Transform topWall;   //上の壁の参照を追加
+    public float totalDepth;    ////縦に並べる座標
+    // Use this for initialization
+    void Start()
+    {
+        //配置する座標を設定
+
+        //Vector3 placePosition = new Vector3(-10, 0, 0);
+
+        Vector3 placePosition = new Vector3(
+          topWall.position.x - topWall.localScale.x / 2 + blockPrefab.transform.localScale.x / 2,
+          topWall.position.y - topWall.localScale.y / 2 - blockPrefab.transform.localScale.y /2 -1 ,
+          0);
+
+        //配置する回転角を設定
+        Quaternion q = new Quaternion();
+        q = Quaternion.identity;
+
+        //幅と奥行きを調整
+        Vector3 localscale = blockPrefab.transform.localScale;
+        localscale.x = topWall.localScale.x / placeX;
+        localscale.y = totalDepth / placeY;
+        blockPrefab.transform.localScale = localscale;
+
+        ////配置
+        //for (int i = 0; i < placeX; i++)
+        //    {
+        //        Instantiate(blockPrefab, placePosition, q);
+        //        placePosition.x += blockPrefab.transform.localScale.x + 1;
+        //    }
+        //配置
+        for (int i = 0; i < placeY; i++)
+        {
+            Vector3 currentPlacePosition
+                = placePosition
+                - Vector3.up * blockPrefab.transform.localScale.y * i;
+            for (int j = 0; j < placeX; j++)
+            {
+                Instantiate(blockPrefab, currentPlacePosition, q);
+                currentPlacePosition.x += blockPrefab.transform.localScale.x;
+            }
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
