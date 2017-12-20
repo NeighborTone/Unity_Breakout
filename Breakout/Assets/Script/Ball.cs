437159bdfeb1cfad4c7b1f73d0af2017c00f324b
R422 <R422@jec.ac.jp>
2017-12-20 11:14:24 +0900

    firstcommit

---
+++ Breakout/Assets/Script/Ball.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour
+{
+    private float speed = 10.0f;    //これを追加
+   
+
+    // Use this for initialization
+    void Start()
+    {
+        this.GetComponent<Rigidbody>().AddForce(
+            (transform.right + transform.up) * speed,
+            ForceMode.VelocityChange);
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+        if (collision.gameObject.tag == "Block")
+        {
+            Destroy(collision.gameObject);
+        }
+        
+        //if (Mathf.Abs(this.GetComponent<Rigidbody>().velocity.y) < 3)
+        //{
+        //    this.GetComponent<Rigidbody>().velocity.y
+        //}
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Breakout/Assets/Script/Bar.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bar : MonoBehaviour
+{
+    // 位置座標
+    private Vector3 position;
+    // スクリーン座標をワールド座標に変換した位置座標
+    private Vector3 screenToWorldPointPosition;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Vector3でマウス位置座標を取得する
+        position = Input.mousePosition;
+        // Z軸修正
+        position.z = -Camera.main.transform.position.z;
+        position.y = -Camera.main.transform.position.y + 60;
+        // マウス位置座標をスクリーン座標からワールド座標に変換する
+        screenToWorldPointPosition = Camera.main.ScreenToWorldPoint(position);
+        // ワールド座標に変換されたマウス座標を代入
+        gameObject.transform.position = screenToWorldPointPosition;
+    }
+}
