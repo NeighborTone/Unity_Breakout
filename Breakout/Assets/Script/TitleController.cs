948736d61a16a9bef530f8a10e660f9d4e2ef8a0
tonarinohito <rsp44316@gmail.com>
2017-12-21 01:29:04 +0900

    一通り完成

---
+++ Breakout/Assets/Script/TitleController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleController : MonoBehaviour {
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Z))
+        {
+            SceneManager.LoadScene("Breakout");
+        }
+        
+    }
+}
---
+++ Breakout/Breakout_Unity/Assets/Script/Bar.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bar : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        if(Input.GetKey(KeyCode.RightArrow) && transform.position.x <= 10)
+        {
+            transform.position += new Vector3(23.0f, 0.0f, 0.0f) * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) && transform.position.x >= -10)
+        {
+            transform.position += new Vector3(-23.0f, 0.0f, 0.0f) * Time.deltaTime;
+        }
+    }
+}
